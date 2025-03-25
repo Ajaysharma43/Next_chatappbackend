6573730bd7ca596e828fef746d900f2cd52f49bd
ajay sharma <ajaysharma445446@gmail.com>
2025-03-25 16:53:40 +0530

    changes

--- Controllers/AuthControllers/VerifyOpt.js
+++ Controllers/AuthControllers/VerifyOpt.js
@@ -1,6 +1,18 @@
-const VerifyOpt = (req , res , next) => {
+import bcrypt from 'bcrypt'
+
+export const VerifyOtp = (req , res , next) => {
     try {
-        const {OPT} = req.body;
+        const {OTP , otp} = req.body;
+        bcrypt.compare(otp , OTP , function(err , result){
+            if(result == true)
+            {
+                res.status(200).json({success : true})
+            }
+            else
+            {
+                res.status(200).json({success : true})
+            }
+        })
     } catch (error) {
         res.status(404).json({error : error})
     }
--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -2,6 +2,7 @@ import express, { Router } from 'express'
 import { CreateUser } from '../Controllers/AuthControllers/SignupControllers.js';
 import { CheckUser } from '../Controllers/AuthControllers/EmailVerifycontroller.js';
 import { GenerateOtp } from '../Controllers/AuthControllers/OtpVerifyController.js';
+import { VerifyOtp } from '../Controllers/AuthControllers/VerifyOpt.js';
 
 const router = express.Router();
 
@@ -9,6 +10,6 @@ const router = express.Router();
 router.post('/Signup' , CreateUser)
 router.post('/Checkuser' , CheckUser)
 router.post('/OTPgenerate' , GenerateOtp)
-router.post('/VerifyOpt' ,  )
+router.post('/VerifyOtp' , VerifyOtp )
 
 export default router
