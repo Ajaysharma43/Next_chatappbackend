4ea5a35377dfda36ef0570727c728d632abd1082
ajay sharma <ajaysharma445446@gmail.com>
2025-04-01 18:01:59 +0530

    changes

--- Controllers/DashBoardControllers/CrudOperations.js
+++ Controllers/DashBoardControllers/CrudOperations.js
@@ -43,7 +43,6 @@ export const UpdateUser = async (req, res, next) => {
 export const DeleteUser = async (req, res, next) => {
     try {
         const { id } = req.query;
-        console.log(`user id is ${id}`)
         const user = await pool.query(`
             SELECT * FROM users
             WHERE id = $1
@@ -67,32 +66,32 @@ export const DeleteUser = async (req, res, next) => {
 
 export const AddUser = async (req, res, next) => {
     try {
-      const { UserData } = req.body;
-  
-      // Hash the password with bcrypt
-      const saltRounds = 10;
-      const hashedPassword = await bcrypt.hash(UserData.password, saltRounds);
-  
-      // Insert user into the database
-      const result = await pool.query(
-        `INSERT INTO users (name, email, phone, street, city, country, postal_code, password) 
+        const { UserData } = req.body;
+
+        // Hash the password with bcrypt
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(UserData.password, saltRounds);
+
+        // Insert user into the database
+        const result = await pool.query(
+            `INSERT INTO users (name, email, phone, street, city, country, postal_code, password) 
          VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
-        [
-          UserData.name,
-          UserData.email,
-          UserData.phone,
-          UserData.street,
-          UserData.city,
-          UserData.country,
-          UserData.postal_code,
-          hashedPassword, // Store the hashed password instead of plain text
-        ]
-      );
-  
-      res.status(201).json({ message: "User added successfully", user: result.rows , Success : true });
-  
+            [
+                UserData.name,
+                UserData.email,
+                UserData.phone,
+                UserData.street,
+                UserData.city,
+                UserData.country,
+                UserData.postal_code,
+                hashedPassword, // Store the hashed password instead of plain text
+            ]
+        );
+
+        res.status(201).json({ message: "User added successfully", user: result.rows, Success: true });
+
     } catch (error) {
-      console.error("Error adding user:", error);
-      res.status(500).json({ error: "Internal Server Error" });
+        console.error("Error adding user:", error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
-  };
+};
