dca07a5ac677f87e303710b335c4d13cdbd156b6
ajay sharma <ajaysharma445446@gmail.com>
2025-04-08 12:49:58 +0530

    changes

--- Controllers/SocketControllers/GetRequestsControllers.js
+++ Controllers/SocketControllers/GetRequestsControllers.js
@@ -3,13 +3,16 @@ import pool from "../../Databaseconnection/DBConnection.js";
 export const GetRequestController = async (req, res, next) => {
     try {
         const { senderid } = req.query;
-        const Requests = await pool.query(`
+        const SendRequests = await pool.query(`
             SELECT * FROM requests
-            WHERE sender_id = $1 
-            OR receiver_id = $1
+            WHERE sender_id = $1
             `,[senderid])
 
-            res.status(200).json({Requests : Requests.rows , success : true})
+            const ReciveRequests = await pool.query(`
+                SELECT * FROM requests
+            WHERE receiver_id = $1
+                `,[senderid])
+            res.status(200).json({SendRequests : SendRequests.rows , ReciveRequests : ReciveRequests.rows , success : true})
     } catch (error) {
         res.status(400).json({ message: "error occured while fetching the requests", error: error })
     }
