bb52ee9390252ceea873dc4495d5a3c7bf043c95
ajay sharma <ajaysharma445446@gmail.com>
2025-04-10 11:42:02 +0530

    changes

--- Controllers/SocketControllers/UnreadMessagesControllers.js
+++ Controllers/SocketControllers/UnreadMessagesControllers.js
@@ -1,15 +1,23 @@
 import pool from "../../Databaseconnection/DBConnection.js"
 
-export const UnreadMessages = async(req , res , next) => {
+export const UnreadMessages = async (req, res, next) => {
     try {
-        const {sender , receiver} = req.body
+        const { sender, receiver } = req.body
         const UnreadMessagesData = await pool.query(`
-            SELECT COUNT(messagestatus) FROM personalchat
-            WHERE sender = $2 AND receiver = $1 AND messagestatus = $3
-            `,[sender , receiver , false])
+            SELECT sender, COUNT(messagestatus) 
+FROM personalchat
+WHERE (
+    (sender = $2 AND receiver = $1) 
+    OR 
+    (sender = $1 AND receiver = $2)
+) 
+AND messagestatus = $3
+GROUP BY sender
 
-            res.status(200).json({message : "unread messages is successfully get" , UnreadMessages : UnreadMessagesData.rows})
+
+            `, [sender, receiver, false])
+        res.status(200).json({ message: "unread messages is successfully get", UnreadMessages: UnreadMessagesData.rows })
     } catch (error) {
-        res.status(400).json({message : "failed of getting unread messages" , error : error})
+        res.status(400).json({ message: "failed of getting unread messages", error: error })
     }
 }
