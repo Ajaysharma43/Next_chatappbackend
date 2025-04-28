d4fc6378e131e9a5b177975a383c57975eee9351
ajay sharma <ajaysharma445446@gmail.com>
2025-04-28 17:37:36 +0530

    changes

--- Controllers/FireBaseControllers/GetCommentsControllers.js
+++ Controllers/FireBaseControllers/GetCommentsControllers.js
@@ -1,8 +1,8 @@
 import pool from "../../Databaseconnection/DBConnection.js"
 
-export const GetComments = async (req , res , next) => {
+export const GetComments = async (req, res, next) => {
     try {
-        const {imageid , userid} = req.query
+        const { imageid } = req.query
         const Comments = await pool.query(`
             SELECT 
             image_comments.id,
@@ -16,9 +16,9 @@ export const GetComments = async (req , res , next) => {
             INNER JOIN users ON users.id = image_comments.user_id
             WHERE image_comments.image_id = $1
             ORDER BY image_comments.created_at ASC
-            `,[imageid])
-            res.status(200).json({message : 'comments data is successfully fetched' , success : true , Comments : Comments.rows})
+            `, [imageid])
+        res.status(200).json({ message: 'comments data is successfully fetched', success: true, Comments: Comments.rows })
     } catch (error) {
-        res.status(404).json({message : "error while fetching comment from db" , success : false , error : error})
+        res.status(404).json({ message: "error while fetching comment from db", success: false, error: error })
     }
 }
--- Controllers/FireBaseControllers/ImageCommentController.js
+++ Controllers/FireBaseControllers/ImageCommentController.js
@@ -4,19 +4,25 @@ export const ImageCommentController = async (req, res, next) => {
     try {
         const { imageid, userid, comment } = req.body;
         const CommentOnImage = await pool.query(`
-            INSERT INTO image_comments(image_id , user_id , comment)
-            VALUES($1 , $2 , $3)
+            WITH inserted_comment AS (
+            INSERT INTO image_comments (image_id, user_id, comment)
+            VALUES ($1, $2, $3)
             RETURNING *
-            `,[imageid , userid , comment])
-            if(CommentOnImage.rowCount == 1)
-            {
-                res.status(200).json({message : "comment is successfully added on the image" , success : true , Comment : CommentOnImage.rows})
-            }
-            else
-            {
-                res.status(404).json({message : "failed to add comment on image" , success : false})
-            }
-            
+            )
+            SELECT 
+            inserted_comment.*,
+            users.name,
+            users.profilepic
+            FROM inserted_comment
+            JOIN users ON users.id = inserted_comment.user_id;
+            `, [imageid, userid, comment])
+        if (CommentOnImage.rowCount == 1) {
+            res.status(200).json({ message: "comment is successfully added on the image", success: true, Comment: CommentOnImage.rows })
+        }
+        else {
+            res.status(404).json({ message: "failed to add comment on image", success: false })
+        }
+
     } catch (error) {
         res.status(404).json({ message: "error while adding comment to the image", success: false, error: error })
     }
